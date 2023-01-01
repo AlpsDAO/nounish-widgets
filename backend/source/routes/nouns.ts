a101343c848c2af82eea227452ee1d837861006a
Benbodhi <5193411+benbodhi@users.noreply.github.com>
2023-01-02 00:36:50 +1100

    add alps route, controller and widget

--- backend/source/routes/nouns.ts
+++ backend/source/routes/nouns.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import nounsController from '../controllers/nouns'
 import lilNounsController from '../controllers/lilnouns'
+import alpsController from '../controllers/alps'
 
 const router = express.Router()
 
 router.get('/nouns', nounsController.getNounsData)
 router.get('/lil-nouns', lilNounsController.getLilNounsData)
+router.get('/alps', alpsController.getAlpsData)
 
 export = router
